e57c3af1852404c837ac7e0c9707abf967461fa8
Rickey Richardson <richardson.rickey92@icloud.com>
2022-09-23 19:23:37 -0400

    question 1

---
+++ assignments/week 1/bash1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+string='0'
+
+echo 0
+echo 0 1
+echo 0 1 2
+echo 0 1 2 3
+echo 0 1 2 3 4
+echo 0 1 2 3 4 5
+#echo string
+#x = 0
+
+#while x<=5;
+#do
+#string+=x
+#echo string
+#x+=1
+#done
